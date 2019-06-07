b2731536a4f1a517bfc868dcf98095f9d184db59
dattasai175 <datta.sai175@gmail.com>
2019-06-07 11:56:59 +0530

    added database

--- grievance/settings.py
+++ grievance/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'complaint.apps.ComplaintConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
