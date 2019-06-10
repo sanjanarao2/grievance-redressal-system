0044eec35086a5cc536adb0769e18ea9d2d65b2b
mrunalinir <rmruna.9999@gmail.com>
2019-06-10 13:49:50 +0530

    user-registration

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-Grievance Redressal System for ESD
-The portal is made using Django 2.0.2
-It can be run on the command line using command "python3 manage.py runserver"
+Grievance Redressal System for ESD.
+The portal is made using Django 2.0.2.
+It can be run on the command line using command "python3 manage.py runserver".
+
+Superuser:
+  username: testing
+  password: esd@telwap
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
@@ -11,4 +12,12 @@ def login(request):
     return render(request, 'login.html')
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method== "POST":
+        form = UserCreationForm()
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for { username }')
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {"form" : form })
