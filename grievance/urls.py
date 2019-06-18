9c2822170e59c6bb885672bf0c4674e1e88bb4b7
mrunalinir <rmruna.9999@gmail.com>
2019-06-18 17:11:22 +0530

    .

--- grievance/urls.py
+++ grievance/urls.py
@@ -18,4 +18,10 @@ urlpatterns = [
     path('dashboard/', compviews.dashboard, name='complaint-dashboard'),
     path('done/', compviews.done, name='complaint-registered'),
 
+    path('password_reset/', auth_views.password_reset, name='password_reset'),
+    path('password_reset/done/', auth_views.password_reset_done, name='password_reset_done'),
+    path('reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',
+        auth_views.password_reset_confirm, name='password_reset_confirm'),
+    path('reset/done/', auth_views.password_reset_complete, name='password_reset_complete'),
+])
 ]
