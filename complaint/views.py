19360b5d6d7b5db4ef19dc65e34797057a32d978
dattasai175 <datta.sai175@gmail.com>
2019-06-18 16:27:11 +0530

    dattasai175

--- complaint/views.py
+++ complaint/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,get_object_or_404,redirect
 from django.utils import timezone
 from django.http import HttpResponse
 from .models import Complaint
-from .forms import ComplaintForm
+from .forms import ComplaintForm,editprofileform,complaintredressal
 from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model,update_session_auth_hash
+from django.contrib.auth.forms import UserChangeForm,PasswordChangeForm
+
+
 
 from django.contrib.auth.decorators import login_required
+
+
 
 # Create your views here.
 @login_required
@@ -35,6 +41,45 @@ def dashboard(request):
     return render(request, 'complaint-dashboard.html', context)
 
 @login_required
+def edit(request):
+    
+    #person = get_user_model()
+    if request.method == "POST":
+        form = editprofileform(request.POST,instance = request.user,initial={'email':request.user.email,'phone':request.user.phone})     
+        form.actual_user = request.user
+        #if form.is_valid():
+        form.save()
+        return redirect('/mycomplaints')
+    else:
+        form = editprofileform(initial={'email':request.user.email,
+            'phone':request.user.phone,
+            'housenumber':request.user.housenumber,
+            'locality':request.user.locality,
+            'village':request.user.village,
+            'mandal':request.user.mandal,
+            'district':request.user.district,
+            'pincode':request.user.pincode})     
+        args = {'form':form}
+        return render(request, 'edit.html', args)
+
+
+@login_required
+def passwordchange(request):
+    
+    if request.method == "POST":
+        form = PasswordChangeForm(data=request.POST, user=request.user)     
+        
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            return redirect('/mycomplaints')
+    else:
+        form = PasswordChangeForm(user=request.user)     
+        args = {'form':form}
+        return render(request, 'edit-password.html', args)
+
+
+@login_required
 def mycomplaints(request):
     context = {
         'details' : request.user,
@@ -49,3 +94,20 @@ def done(request):
     "form": context
         }
     return render(request, 'complaint-registered.html', content)
+
+
+@login_required
+def redressal(request, cmp_id):
+    comp = get_object_or_404(Complaint,pk=cmp_id)
+    if request.method == "POST":
+        form =complaintredressal(request.POST)
+        if form.is_valid():  
+            comp.status = form.cleaned_data['status']
+            comp.resolution = form.cleaned_data['resolution'] 
+            comp.resolved_by = request.user.username
+            comp.save()
+            return redirect('/dashboard') 
+    
+    form = complaintredressal()
+
+    return render(request, 'complaint-redressal.html',{'comp':comp,'form':form})
